ffc985c594e4587409b375f66583a8e11ba86f1e
corbinq27 <corbinjohnson@me.com>
2017-06-19 22:24:56 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+
+const Alexa = require('alexa-sdk');
+
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+const languageStrings = {
+    'en': {
+        translation: {
+            FACTS: [
+                'A year on Mercury is just 88 days long.',
+                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
+                'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
+                'On Mars, the Sun appears about half the size as it does on Earth.',
+                'Earth is the only planet not named after a god.',
+                'Jupiter has the shortest day of all the planets.',
+                'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
+                'The Sun contains 99.86% of the mass in the Solar System.',
+                'The Sun is an almost perfect sphere.',
+                'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
+                'Saturn radiates two and a half times more energy into space than it receives from the sun.',
+                'The temperature inside the Sun can reach 15 million degrees Celsius.',
+                'The Moon is moving approximately 3.8 cm away from our planet every year.',
+            ],
+            SKILL_NAME: 'Space Facts',
+            GET_FACT_MESSAGE: "Here's your fact: ",
+            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+            HELP_REPROMPT: 'What can I help you with?',
+            STOP_MESSAGE: 'Goodbye!',
+        },
+    },
+    'en-US': {
+        translation: {
+            FACTS: [
+                'There were 201 cards in the Voyager set!',
+                'The only equipment card found in the Enterprise Collection was the Starfleet Phase Pistol.',
+                'A shuttlecraft can be found in the image of the card Mona Lisa.',
+                'Locutus of Borg has a Yuta number of 8.',
+                'The only Rare site cards are Garaks Tailor Shop and Quarks Bar.',
+                'On the card Red Alert, the Decipher offices ZIP code is diplayed in the image.',
+                'The word MAX appears on the sweater worn by Berlingoff Rasmussen.',
+                'Several cards have special text in the copywrite notice.  One such card is Paul Rice.',
+                'The card Space was originally to have the lore As of 2370 the Federation had charted 19% of the galaxy, most of it empty space.',
+                'The Card Lemon Aid was originally titled Q Toast.',
+                'Considerations had been given to call Wesley Gets the Point Die, Wesley, Die! named after the famous wesley crusher hating newsgroup.  Cooler heads prevailed.',
+                '162 Million cards were printed for the beta release of the premiere set in 1995',
+                '42335 copies of the non-foil Future Enterprise were printed from Alternate Universe.',
+            ],
+            SKILL_NAME: 'Trek CCG Facts',
+        },
+    },
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random space fact from the space facts list
+        // Use this.t() to get corresponding language data
+        const factArr = this.t('FACTS');
+        const factIndex = Math.floor(Math.random() * factArr.length);
+        const randomFact = factArr[factIndex];
+
+        // Create speech output
+        const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
+        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_MESSAGE');
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
